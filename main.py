0f8567fcfff119ad35566ed89e25e8e1ea0c62d6
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-07-20 16:04:45 +0800

    Add Functionalities

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Open the file and create the Dictionary
+data = pd.read_csv("nato_phonetic_alphabet.csv")
+data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+# Ask for input
+inputted_words = input("Enter a word: ").upper()
+
+# Create a list of Code Words
+result = [data_dict[letter] for letter in inputted_words]
+print(result)
