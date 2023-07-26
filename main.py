b77ecb9b37ddc5d361c3c4ba9c1c8b0daf49c234
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-07-26 19:32:54 +0800

    Add error handling

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import pandas as pd
 
-# Open the file and create the Dictionary
-data = pd.read_csv("Day 26/Nato Alphabet/nato_phonetic_alphabet.csv")
+# Open the file and create a Dictionary
+data = pd.read_csv("nato_phonetic_alphabet.csv")
 data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(data_dict)
 
 # Ask for input
-inputted_words = input("Enter a word: ").upper()
+
 
 # Create a list of Code Words
-result = [data_dict[letter] for letter in inputted_words]
-print(result)
+while True:
+    inputted_words = input("Enter a word: ").upper()
+
+    try:
+        result = [data_dict[letter] for letter in inputted_words]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+    else:
+        print(result)
+        break
